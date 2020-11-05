86cbe3de4d9bd71e170296267f5f863c7c059d25
Angel Shamieh <angellshamieh5@gmail.com>
2020-11-05 16:52:33 +0100

    update the gem file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,10 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
+  # other config
+  config.public_file_server.enabled = true
+
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
